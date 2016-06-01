35199e9c2429c6ab1e5e8f7925f5120410463ce6
Rony Nasr and Sebastian Prokuski <sebastian@>
2016-06-01 10:29:11 -0700

    add front-end usability to calculator

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// Business (or back-end) logic:
+
+var add = function (num1,num2){
+  return num1+num2;
+}
+var subtract = function (num1,num2){
+  return num1-num2;
+}
+var multiply = function (num1,num2){
+  return num1*num2;
+}
+var divide = function (num1,num2){
+  return num1/num2;
+}
+
+// User interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+  event.preventDefault();
+  var num1 = parseInt($("#add1").val());
+  var num2 = parseInt($("#add2").val());
+  var result = add(num1,num2);
+  $("#output").text(result);
+  });
+});
